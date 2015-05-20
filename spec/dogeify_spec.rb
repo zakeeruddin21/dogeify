7252859868ae2f8b3bf1e784a5030987ca610978
Zakeeruddin <zakeeruddin_mohammed@gap.com>
2015-05-19 19:38:03 -0700

    first commit

---
+++ spec/dogeify_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Dogeify do
+  it 'has a version number' do
+    expect(Dogeify::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
